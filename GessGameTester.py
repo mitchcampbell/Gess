ba2ce92cf3a51592a92956cc3471c2a2d1974cd0
Mitch <39753367+mitchcampbell@users.noreply.github.com>
2020-06-08 14:45:30 -0400

    Added initial files

---
+++ GessGameTester.py
@@ -0,0 +1,25 @@
+import unittest
+import GessGame as gg
+
+
+class GessGameTester(unittest.TestCase):
+
+    def setUp(self):
+
+        self.g0 = gg.GessGame()
+
+        self.g1 = gg.GessGame()
+        self.g1.make_move("c8", "c11")
+        self.g1.make_move("l14", "r8")
+        self.g1.make_move("m6", "j9")
+
+    def test_game_init(self):
+
+        self.assertEqual("UNFINISHED", self.g0.get_game_state())
+        self.assertEqual("BLACK", self.g0.get_current_player())
+
+        self.assertIsNone(self.g0._board.winner)
+
+    def test_f07_f11_black(self):
+
+        self.assertEqual(False, self.g0.make_move("f07", "f11"))
---
+++ _gessgame_funcs.py
@@ -0,0 +1,10 @@
+def tile_translate(y, x):
+    """
+    Translates the given (y, x) coordinate pair of a user's mouse click
+    to the format of a tile on a Gess game board
+
+    Example
+    :param y: 350
+    :param x: 415
+    :return:
+    """
